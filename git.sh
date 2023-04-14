ae27fb0acc2aac10be67ad9982f836d5322cdf3b
Asteroidus <113026499+AstroidusTv@users.noreply.github.com>
2023-04-14 23:04:09 +0200

    Add desktop file and new version of bash file

--- git.sh
+++ git.sh
@@ -1,10 +1,21 @@
-	 #!/bin/bash
-
+#!/bin/bash
 
 echo "Message for commit : "
 read message
-cd /home/achille/Documents/Projects/Html/Cescosite/
+
+echo "Enter branch name (default: main) : "
+read branch
+
+# Vérifier si branch est vide, alors utiliser la branche par défaut "main"
+if [ -z "$branch" ]
+then
+    branch="main"
+fi
+
+cd /home/achille/Documents/Programation/Html/Cescosite/
+git checkout $branch
 git add .
-git commit -m "$message" 
+git commit -m "$message"
 echo $message
-git push
+git push origin $branch
+
